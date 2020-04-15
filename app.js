733e4e5f9f842322b60c83d433af881feabcffd1
Drew Radcliff <sirampr@gmail.com>
2020-04-15 14:11:45 -0400

    add file uploading and display images to page

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + ".jpg";
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const app = express();
+const port = 3000;
+const uploaded_files = [];
+
+app.get("/", (req, res) => {
+  const path = "./public/uploads";
+  fs.readdir(path, (err, items) => {
+    const images = items.map((image) => {
+      return `<img style="height:50px" src="./uploads/${image}" />`;
+    });
+    res.send(`
+      <h1>Welcome to Kenziegram!</h1>
+      ${images}
+      <form
+      action="/upload"
+      method="POST"
+      enctype="multipart/form-data"
+    >
+      <input type="file" name="myFile" />
+      <button type="submit">Upload</button>
+    </form>
+    `);
+  });
+});
+
+app.post("/upload", upload.single("myFile"), function (
+  request,
+  response,
+  next
+) {
+  uploaded_files.push(request.file.filename);
+  response.redirect("/");
+});
+
+app.use(express.static("./public"));
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
