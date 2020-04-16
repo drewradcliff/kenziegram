39306713f36a2d95bfa8a962973ee13ede52ac23
Drew Radcliff <sirampr@gmail.com>
2020-04-16 12:09:20 -0400

    Add template engines with pug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const multer = require("multer");
+const pug = require("pug");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -13,42 +14,22 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
 const app = express();
 const port = 3000;
 const uploaded_files = [];
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
   const path = "./public/uploads";
   fs.readdir(path, (err, items) => {
-    const images = items.map((image) => {
-      return `<img style="height:100px" src="./uploads/${image}" />`;
-    });
-    res.send(`
-      <h1>Welcome to Kenziegram!</h1>
-      ${images}
-      <form
-      action="/upload"
-      method="POST"
-      enctype="multipart/form-data"
-    >
-      <input type="file" name="myFile" />
-      <button type="submit">Upload</button>
-    </form>
-    `);
+    res.render("index", { title: "Kenziegram", items });
   });
 });
 
-app.post("/upload", upload.single("myFile"), function (
-  request,
-  response,
-  next
-) {
-  uploaded_files.push(request.file.filename);
-  response.send(`
-    <h1>Uploaded file!</h1>
-    <a href="/">Back Home</a>
-  `);
+app.post("/upload", upload.single("myFile"), function (req, res) {
+  uploaded_files.push(req.file.filename);
+  res.render("fileUploaded");
 });
 
 app.use(express.static("./public"));
