5ca8fc0612f49adafe916a4aa59b7680bb667b94
Drew Radcliff <sirampr@gmail.com>
2020-04-17 15:02:35 -0400

    Add error handling

--- public/polling.js
+++ public/polling.js
@@ -9,8 +9,26 @@ async function getLatest(url = "", data = {}) {
   return response.json();
 }
 
-setInterval(() => {
-  getLatest("/latest", { after: 1523972111977 }).then((data) => {
-    console.log(data);
-  });
+let errCount = 0;
+let interval = setInterval(() => {
+  getLatest("/latest", { after: 1523972111977 })
+    .then((data) => {
+      errCount = 0;
+    })
+    .catch((error) => {
+      errCount++;
+      console.log("Error:", errCount, error);
+      if (errCount == 2) {
+        displayError();
+        clearInterval(interval);
+      }
+    });
 }, 5000);
+
+const displayError = () => {
+  console.log("lost connection");
+  let error = document.createElement("p");
+  error.style.color = "red";
+  error.textContent = "Lost connection to server";
+  document.body.appendChild(error);
+};
